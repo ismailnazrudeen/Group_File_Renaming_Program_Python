402b19a677f439edd67baa7de604087a010565d7
Mohammed Ismail N <51046380+ismailnazrudeen@users.noreply.github.com>
2023-07-12 21:24:23 +0530

    Add files via upload

---
+++ Group_File_Renaming_Program_Python.py
@@ -0,0 +1,70 @@
+'''Group_File_Renaming_Program_Python'''
+
+'''
+> - A Command line application to rename the files and subdirectorie's names inside a given directory in group.
+> - When providing a Directory path, Renames the contents of the Directory with the new name provided by the user.
+> - Takes care of renaming the files with correct extensions. 
+> - User can Undo the rename operation but (only once).
+> - Undoing process should be done on the same execution space as the renaming process.
+
+'''
+
+#OS Module
+import os
+import time
+
+def group_file_rename(path):
+    ''' Renames the names of the files in the given path with the given New Name with the extensions intact'''
+    #List of directories in the path
+    dir_tree = path.split('\\')    
+
+    #Changing Directory to the provided path
+    os.chdir(path)
+
+    file_list = os.listdir()
+    if len(file_list) == 0:
+        return 'The Directory is Empty!!'
+
+    #New Name of the file
+    new_name = input('Enter the New File Name: ').strip()
+
+    #To adjust the file numbers at the end of filename according to the no of files in a directory
+    length = len(str(len(file_list)))
+    j = 1
+        
+    #Looping over the list of files and renaming according to the files' extensions.
+    for i in file_list:
+        #if the current item is a directory, then No Extension is needed.
+        if os.path.isdir(i):
+            os.rename(i, (new_name + '_') + str(j).rjust(length, '0'))
+
+        #if the current item is a File, then extract the extension and append it to the new file name
+        elif os.path.isfile(i):
+            #To store the extension of a particular file to avoid inconsistent renaming of file extensions
+            extension = '.' + i.split('.')[-1]
+            os.rename(i, (new_name + '_') + str(j).rjust(length, '0') + extension)
+
+        j += 1
+    
+    
+#************************************************************************#
+    ''' Code for Undoing the Rename Operation '''
+    choice = input('\nUNDO Renaming ? (Y/N): ')
+
+    if choice in ('y', 'Y'):
+        file_list_new = os.listdir()
+        
+        for i in range(len(file_list_new)):
+            os.rename(file_list_new[i], file_list[i])
+        
+        return '***************** UNDO Completed. *****************'
+
+    return '***************** File Renaming Completed. *****************'
+    
+
+#Path where you want to rename the files
+path = input('Enter Path: ').strip()
+
+print(group_file_rename(path))
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Group_File_Renaming_Program_Python
+> - A Command line application to rename the files and subdirectorie's names inside a given directory in group.
+> - When providing a Directory path, Renames the contents of the Directory with the new name provided by the user.
+> - Takes care of renaming the files with correct extensions. 
+> - User can Undo the rename operation but (only once).
+> - Undoing process should be done on the same execution space as the renaming process.
